8c529880a3db89ff95250afce21055a2424f1e85
JJPadilla <jjpadilla0915@gmail.com>
2021-01-03 02:27:52 +0800

    added taskbar

---
+++ RogueBaker/Assets/Scripts/Utilities/Taskbar.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Taskbar : MonoBehaviour
+{
+    private Animator anime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anime = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void toggleTasks()
+    {
+        Debug.Log("toggled");
+        if (anime.GetBool("openTasks"))
+        {
+            anime.SetBool("openTasks", false);
+        }
+        else
+        {
+            anime.SetBool("openTasks", true);
+        }
+        
+    }
+}
